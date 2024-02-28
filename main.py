eae65d62e10dd127f1180fc935f47007012ba675
Алан Джелиев <djalan84@mail.ru>
2024-02-29 00:05:13 +0300

    Refactoring: Comments added

--- main.py
+++ main.py
@@ -11,12 +11,36 @@ logging.basicConfig(level=logging.INFO)
 
 
 class Employee:
+    """
+    A class representing an employee.
+
+    Args:
+        full_name (str): The full name of the employee.
+        birth_date (str): The birthdate of the employee in the format YYYY-MM-DD.
+        gender (str): The gender of the employee, either "Male" or "Female".
+
+    Attributes:
+        id (int): The unique ID of the employee.
+        full_name (str): The full name of the employee.
+        birth_date (str): The birthdate of the employee in the format YYYY-MM-DD.
+        gender (str): The gender of the employee, either "Male" or "Female".
+
+    """
+
     def __init__(self, full_name, birth_date, gender):
+        self.id = None
         self.full_name = full_name
         self.birth_date = birth_date
         self.gender = gender
 
-    def age(self):
+    def age(self) -> int:
+        """
+        Calculates the age of the employee.
+
+        Returns:
+            int: The age of the employee.
+
+        """
         today = datetime.now()
         birth_date = datetime.strptime(
             self.birth_date,
@@ -26,10 +50,23 @@ class Employee:
         return age
 
     def save(self, db):
+        """
+        Saves the employee to the database.
+
+        Args:
+            db (Database): The database connection object.
+
+        """
         db.add_employee(self)
 
 
-def generate_random():
+def generate_random() -> list[Employee]:
+    """
+    This function generates a list of random Employees.
+
+    Returns:
+        list[Employee]: A list of random Employees.
+    """
     last_names = [
         "Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis"
     ]
@@ -48,7 +85,13 @@ def generate_random():
     return random_employees
 
 
-def generate_special():
+def generate_special() -> list[Employee]:
+    """
+    This function generates a list of 100 random male employees with special last names.
+
+    Returns:
+        list[Employee]: A list of 100 random male employees with special last names.
+    """
     special_last_names = [
         "Fisher", "Ford", "Fletcher", "Farrell", "Faulkner", "Frost"
     ]
@@ -65,7 +108,29 @@ def generate_special():
 
 
 class Database:
+    """
+    A class that represents a database connection.
+
+    Args:
+        dbname (str): The name of the database.
+        user (str): The username for the database.
+        password (str): The password for the database.
+        host (str): The hostname of the database.
+
+    Attributes:
+        conn (psycopg2.Connection): The database connection object.
+        cur (psycopg2.Cursor): The database cursor object.
+
+    """
+
     def __init__(self, dbname, user, password, host):
+        """
+        Initializes the database connection.
+
+        Raises:
+            psycopg2.Error: If there is an error connecting to the database.
+
+        """
         try:
             self.conn = psycopg2.connect(
                 dbname=dbname,
@@ -79,6 +144,13 @@ class Database:
             sys.exit(1)
 
     def create_table(self):
+        """
+        Creates the employees table in the database if it does not already exist.
+
+        Raises:
+            psycopg2.Error: If there is an error creating the table.
+
+        """
         self.cur.execute("""
             CREATE TABLE IF NOT EXISTS employees (
                 id SERIAL PRIMARY KEY,
@@ -90,6 +162,16 @@ class Database:
         self.conn.commit()
 
     def add_employee(self, employee):
+        """
+        Adds an employee to the database.
+
+        Args:
+            employee (Employee): The employee object to be added.
+
+        Raises:
+            psycopg2.Error: If there is an error adding the employee.
+
+        """
         self.cur.execute("""
             INSERT INTO employees (full_name, birth_date, gender)
             VALUES (%s, %s, %s)
@@ -97,6 +179,16 @@ class Database:
         self.conn.commit()
 
     def add_employees_bulk(self, employees):
+        """
+        Adds a list of employees to the database in bulk.
+
+        Args:
+            employees (list[Employee]): The list of employee objects to be added.
+
+        Raises:
+            psycopg2.Error: If there is an error adding the employees.
+
+        """
         buffer = io.StringIO()
         for emp in employees:
             buffer.write(f"{emp.full_name},{emp.birth_date},{emp.gender}\n")
@@ -116,6 +208,13 @@ class Database:
             logging.error(f"Error during bulk insert with copy_from: {e}")
 
     def get_all_employees(self):
+        """
+        Retrieves all employees from the database and prints them to the console.
+
+        Raises:
+            psycopg2.Error: If there is an error retrieving the employees.
+
+        """
         self.cur.execute("""
             SELECT DISTINCT full_name, birth_date, gender,
             EXTRACT(YEAR FROM AGE(birth_date)) AS age
@@ -132,6 +231,17 @@ class Database:
             )
 
     def get_employees_by_criteria(self, gender, first_letter):
+        """
+        Retrieves employees from the database based on a given criteria and prints them to the console.
+
+        Args:
+            gender (str): The gender of the employees to be retrieved.
+            first_letter (str): The first letter of the employees' names to be retrieved.
+
+        Raises:
+            psycopg2.Error: If there is an error retrieving the employees.
+
+        """
         start_time = time.time()
         self.cur.execute("""
             SELECT full_name, birth_date, gender
@@ -142,6 +252,13 @@ class Database:
         logging.info(f"Query Time: {time.time() - start_time} seconds")
 
     def optimize_database(self):
+        """
+        Optimizes the database by creating indexes on the relevant columns.
+
+        Raises:
+            psycopg2.Error: If there is an error optimizing the database.
+
+        """
         self.cur.execute(
             "CREATE INDEX IF NOT EXISTS idx_gender ON employees (gender)"
         )
@@ -151,6 +268,13 @@ class Database:
         self.conn.commit()
 
     def flush_db(self):
+        """
+        Flushes the contents of the database.
+
+        Raises:
+            psycopg2.Error: If there is an error flushing the database.
+
+        """
         try:
             with self.conn.cursor() as cur:
                 cur.execute("DELETE FROM employees")
@@ -161,12 +285,29 @@ class Database:
             logging.error(f"Error flushing database: {e}")
 
     def close(self):
+        """
+        Closes the database connection.
+
+        """
         if self.conn:
             self.cur.close()
             self.conn.close()
 
 
 def main(mode):
+    """
+    Main function of the application.
+
+    Args:
+        mode (str): The mode in which the application is being run. Possible modes are:
+            "1": Create the database table.
+            "2": Add an employee to the database.
+            "3": Retrieve all employees from the database.
+            "4": Add a large number of random employees to the database.
+            "5": Retrieve male employees whose names start with "F" from the database.
+            "6": Optimize the database.
+            "flush": Delete all data from the database.
+    """
     try:
         db = Database(DB_NAME, DB_USER, DB_PASS, DB_HOST)
 
